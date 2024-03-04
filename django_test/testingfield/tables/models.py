ebac2bab51da4ea8c9e6a12e46910bd85628d8f9
abhishek0wb <abhishek0wb@gmail.com>
2024-03-04 15:18:10 +0530

    models

--- django_test/testingfield/tables/models.py
+++ django_test/testingfield/tables/models.py
@@ -7,3 +7,12 @@ class student(models.Model):
     age = models.IntegerField()
     email = models.EmailField()
     address = models.TextField()
+
+class car(models.Model):
+    car_name = models.CharField(max_length=100, default = 1)
+    car_age = models.IntegerField(default=2)
+
+
+    def __str__(self) -> str:
+        return self.car_name
+    
